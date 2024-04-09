ed3adfdc94e5cf6fd3bcf851e6ab2dec7482be8f
Awet Tsegazeab <avvvet@gmail.com>
2024-04-09 03:01:27 -0400

    webrtc full implementation

---
+++ node/main.go
@@ -0,0 +1,301 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/nats-io/nats.go"
+	"github.com/pion/interceptor"
+	"github.com/pion/interceptor/pkg/intervalpli"
+	"github.com/pion/webrtc/v4"
+)
+
+type RouterNode struct {
+	nat            *nats.Conn
+	peerConnection *webrtc.PeerConnection
+	outputTrack    *webrtc.TrackLocalStaticRTP
+
+	pendingCandidates []*webrtc.ICECandidate
+	candidatesMux     sync.Mutex
+}
+
+func main() {
+	n := &RouterNode{
+		pendingCandidates: make([]*webrtc.ICECandidate, 0),
+	}
+
+	n.NatCon()
+
+	n.PeerConnection()
+
+	//regisgter peerConnection events
+
+	n.peerConnection.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+		n.onVideoTrackArrived(track, receiver)
+	})
+
+	n.peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		n.onStateChange(s)
+	})
+
+	n.peerConnection.OnICEConnectionStateChange(func(ic webrtc.ICEConnectionState) {
+		n.onIceStateChange(ic)
+	})
+
+	n.peerConnection.OnICECandidate(func(c *webrtc.ICECandidate) {
+		n.onIceCandidate(c)
+	})
+
+	outputTrack, err := webrtc.NewTrackLocalStaticRTP(webrtc.RTPCodecCapability{
+		MimeType:     webrtc.MimeTypeVP8,
+		ClockRate:    90000,
+		SDPFmtpLine:  "max-fs=12288;max-fr=60",
+		RTCPFeedback: nil,
+	}, "video_"+strconv.FormatUint(7777, 10), strconv.FormatUint(7777, 10))
+	if err != nil {
+		log.Printf("Error%s", err)
+	}
+
+	_, err = n.peerConnection.AddTrack(outputTrack)
+	if err != nil {
+		log.Printf("Error%s", err)
+	}
+	n.outputTrack = outputTrack
+
+	//router subscripes to listen sdp offer
+	subject := "to.router.a.add.sdp"
+	sub, err := n.nat.Subscribe(subject, func(msg *nats.Msg) {
+		n.AddSdp(msg)
+	})
+	if err != nil {
+		log.Printf("unable to create subscription %s : %s", subject, err)
+	}
+	defer sub.Unsubscribe()
+
+	//router subscripes to listen sdp offer
+	subject = "to.router.a.add.candidate"
+	sub, err = n.nat.Subscribe(subject, func(msg *nats.Msg) {
+		n.AddCandidate(msg)
+	})
+	if err != nil {
+		log.Printf("unable to create subscription %s : %s", subject, err)
+	}
+	defer sub.Unsubscribe()
+
+	fmt.Printf("router node is running")
+	select {}
+}
+
+func (n *RouterNode) AddCandidate(msg *nats.Msg) {
+	candidate := webrtc.ICECandidateInit{}
+	err := json.Unmarshal(msg.Data, &candidate)
+	if err != nil {
+		log.Printf("Error unmarshal %s", err)
+	}
+
+	err = n.peerConnection.AddICECandidate(candidate)
+	if err != nil {
+		log.Printf("Error adding candidate %s", err)
+	}
+
+	log.Printf("candidate added %s", err)
+}
+
+func (n *RouterNode) AddSdp(msg *nats.Msg) {
+	sdp := webrtc.SessionDescription{}
+
+	err := json.Unmarshal(msg.Data, &sdp)
+	if err != nil {
+		log.Printf("Error unmarshal %s", err)
+	}
+
+	if err := n.peerConnection.SetRemoteDescription(sdp); err != nil {
+		panic(err)
+	}
+
+	// Create an answer to send to the other process
+	answer, err := n.peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Send the answer to signaling -> web client
+	payload, err := json.Marshal(answer)
+	if err != nil {
+		panic(err)
+	}
+
+	subject := "to.client.a.send.sdp.answer"
+	err = n.nat.Publish(subject, payload)
+	if err != nil {
+		log.Printf("Error publishing subject %s: %s", subject, err)
+		// Handle error
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = n.peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		panic(err)
+	}
+
+	// send pending candidates
+	subject = "to.client.a.send.candidate"
+	n.candidatesMux.Lock()
+	for _, c := range n.pendingCandidates {
+		log.Println("send peding candidate ")
+		payload, err := json.Marshal(c)
+		if err != nil {
+			log.Printf("Error%s", err)
+		}
+
+		err = n.nat.Publish(subject, payload)
+		if err != nil {
+			log.Printf("Error publishing subject %s: %s", subject, err)
+			// Handle error
+		}
+
+	}
+	n.candidatesMux.Unlock()
+
+	log.Println("add sdp process completed")
+}
+
+func (n *RouterNode) NatCon() {
+	url := os.Getenv("NATS_URL")
+	if url == "" {
+		url = "nats://localhost:4222"
+	}
+
+	nc, err := nats.Connect(url)
+	if err != nil {
+		log.Printf("Error: unbale to connect to NATS server %v", err)
+	}
+
+	log.Printf("Connected to NATS server at %s", url)
+
+	// Close the NATS connection when done
+	//defer nc.Close()
+
+	n.nat = nc
+}
+
+func (n *RouterNode) PeerConnection() {
+	m := &webrtc.MediaEngine{}
+
+	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeVP8, ClockRate: 90000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
+		PayloadType:        96,
+	}, webrtc.RTPCodecTypeVideo); err != nil {
+		panic(err)
+	}
+
+	i := &interceptor.Registry{}
+
+	if err := webrtc.RegisterDefaultInterceptors(m, i); err != nil {
+		panic(err)
+	}
+
+	intervalPliFactory, err := intervalpli.NewReceiverInterceptor()
+	if err != nil {
+		panic(err)
+	}
+	i.Add(intervalPliFactory)
+
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(m), webrtc.WithInterceptorRegistry(i))
+
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+			{
+				URLs:       []string{"turn:turn.webrtcwire.online"},
+				Username:   "webrtcwire",
+				Credential: "yellowgreen",
+			},
+		},
+	}
+
+	peerConnection, err := api.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+	// defer func() {
+	// 	if cErr := peerConnection.Close(); cErr != nil {
+	// 		fmt.Printf("cannot close peerConnection: %v\n", cErr)
+	// 	}
+	// }()
+
+	n.peerConnection = peerConnection
+}
+
+func (n *RouterNode) onStateChange(s webrtc.PeerConnectionState) {
+	log.Printf("peer connection state has changed: %s\n", s.String())
+	switch s {
+	case webrtc.PeerConnectionStateConnected:
+	case webrtc.PeerConnectionStateFailed:
+	case webrtc.PeerConnectionStateDisconnected:
+		fallthrough
+	case webrtc.PeerConnectionStateClosed:
+	}
+}
+
+func (n *RouterNode) onIceStateChange(ic webrtc.ICEConnectionState) {
+	log.Printf("peer ICE state has changed: %s\n", ic.String())
+
+	switch ic {
+	case webrtc.ICEConnectionStateConnected:
+		log.Println("✅️ ICE connected stream ")
+	case webrtc.ICEConnectionStateCompleted:
+	case webrtc.ICEConnectionStateClosed:
+	}
+}
+
+// When an ICE candidate is available send to the other Pion instance
+func (n *RouterNode) onIceCandidate(c *webrtc.ICECandidate) {
+
+	if c == nil {
+		return
+	}
+
+	n.candidatesMux.Lock()
+	defer n.candidatesMux.Unlock()
+
+	//check if remote description is set and start sending candidates
+	desc := n.peerConnection.RemoteDescription()
+	if desc == nil {
+		n.pendingCandidates = append(n.pendingCandidates, c)
+	} else {
+		payload, err := json.Marshal(c)
+		if err != nil {
+			log.Printf("Error%s", err)
+		}
+		subject := "to.client.a.send.candidate"
+		err = n.nat.Publish(subject, payload)
+		if err != nil {
+			log.Printf("Error publishing subject %s: %s", subject, err)
+			// Handle error
+		}
+	}
+	log.Println("candidate added")
+}
+
+func (n *RouterNode) onVideoTrackArrived(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
+	log.Printf("🎞️  webrtc stream has started, of type %d: %s\n", track.PayloadType(), track.Codec().MimeType)
+
+	for {
+		// Read RTP packets being sent to Pion
+		rtp, _, readErr := track.ReadRTP()
+		if readErr != nil {
+			panic(readErr)
+		}
+
+		if writeErr := n.outputTrack.WriteRTP(rtp); writeErr != nil {
+			panic(writeErr)
+		}
+	}
+}
---
+++ signaling/main.go
@@ -0,0 +1,278 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/nats-io/nats.go"
+	"github.com/pion/webrtc/v4"
+)
+
+type Signaling struct {
+	port                   string
+	upgrader               websocket.Upgrader
+	activeConnections      map[*websocket.Conn]bool
+	activeConnectionsMutex sync.RWMutex
+	activeSubjects         map[*websocket.Conn]string
+	activeSubjectsMu       sync.Mutex
+	wsMutex                sync.Mutex
+
+	nat *nats.Conn
+}
+
+type ClientMessage struct {
+	Type      string                    `json:"type"`
+	Candidate webrtc.ICECandidateInit   `json:"cadidate,omitempty"`
+	Sdp       webrtc.SessionDescription `json:"sdp,omitempty"`
+}
+
+// client sends its sdp offer to router node
+func (s *Signaling) ClientSendSdpOffer(sdp webrtc.SessionDescription) {
+
+	b, err := json.Marshal(sdp)
+	if err != nil {
+		log.Printf("Error%s", err)
+	}
+
+	subject := "to.router.a.add.sdp"
+	err = s.nat.Publish(subject, b)
+	if err != nil {
+		log.Println("Error publishing client sdp message to NATS:", err)
+		// Handle error
+	}
+}
+
+// client sends its ice as soon as it is found
+func (s *Signaling) ClientSendCandidate(ice webrtc.ICECandidateInit) {
+	b, err := json.Marshal(ice)
+	if err != nil {
+		log.Printf("Error%s", err)
+	}
+
+	s.nat.Publish("to.router.a.add.candidate", b)
+	if err != nil {
+		log.Println("Error publishing client ice message to NATS:", err)
+		// Handle error
+	}
+}
+
+func (s *Signaling) handleWs(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	s.activeConnectionsMutex.Lock()
+	s.activeConnections[conn] = true
+	s.activeConnectionsMutex.Unlock()
+
+	defer delete(s.activeConnections, conn)
+
+	//signal subscripes to listen sdp answer
+	subject := "to.client.a.send.sdp.answer"
+	sub, err := s.nat.Subscribe(subject, func(msg *nats.Msg) {
+		s.ForwardAnswer(conn, msg)
+	})
+	if err != nil {
+		log.Printf("unable to create subscription %s : %s", subject, err)
+	}
+	defer sub.Unsubscribe()
+
+	//signal subscripes to listen candidate
+	subject = "to.client.a.send.candidate"
+	sub, err = s.nat.Subscribe(subject, func(msg *nats.Msg) {
+		fmt.Printf("ice received : %+v", string(msg.Data))
+		s.ForwardCandidate(conn, msg)
+	})
+	if err != nil {
+		log.Printf("unable to create subscription %s : %s", subject, err)
+	}
+	defer sub.Unsubscribe()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		// Unmarshal each inbound WebSocket message
+		var (
+			candidate webrtc.ICECandidateInit
+			offer     webrtc.SessionDescription
+		)
+
+		switch {
+		// Attempt to unmarshal as a SessionDescription. If the SDP field is empty
+		// assume it is not one.
+		case json.Unmarshal(msg, &offer) == nil && offer.SDP != "":
+			//send sdp
+			s.ClientSendSdpOffer(offer)
+		case json.Unmarshal(msg, &candidate) == nil && candidate.Candidate != "":
+			//send ice
+			s.ClientSendCandidate(candidate)
+		default:
+			panic("Unknown message")
+		}
+	}
+
+	s.activeConnectionsMutex.Lock()
+	delete(s.activeConnections, conn)
+	s.activeConnectionsMutex.Unlock()
+}
+
+func (s *Signaling) handleWsOld(w http.ResponseWriter, r *http.Request) {
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	s.activeConnectionsMutex.Lock()
+	s.activeConnections[conn] = true
+	s.activeConnectionsMutex.Unlock()
+
+	defer delete(s.activeConnections, conn)
+
+	//signal subscripes to listen sdp answer
+	subject := "to.client.a.send.sdp.answer"
+	sub, err := s.nat.Subscribe(subject, func(msg *nats.Msg) {
+		s.ForwardAnswer(conn, msg)
+	})
+	if err != nil {
+		log.Printf("unable to create subscription %s : %s", subject, err)
+	}
+	defer sub.Unsubscribe()
+
+	//signal subscripes to listen candidate
+	subject = "to.client.a.send.candidate"
+	sub, err = s.nat.Subscribe(subject, func(msg *nats.Msg) {
+		fmt.Printf("ice received : %+v", string(msg.Data))
+		s.ForwardCandidate(conn, msg)
+	})
+	if err != nil {
+		log.Printf("unable to create subscription %s : %s", subject, err)
+	}
+	defer sub.Unsubscribe()
+
+	for {
+
+		var clientMsg ClientMessage
+
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+
+		if err := json.Unmarshal(msg, &clientMsg); err != nil {
+			log.Println("Error unmarshalling client message:", err)
+			continue
+		}
+
+		switch clientMsg.Type {
+		case "sdp":
+			sdp := clientMsg.Sdp
+			//log.Printf("Received message of type '%s' %s ", clientMsg.Type, sdp.SDP)
+
+			s.ClientSendSdpOffer(sdp)
+		case "candidate":
+			candidate := clientMsg.Candidate
+			//log.Printf("Received message of type '%s' %+v ", clientMsg.Type, candidate)
+
+			s.ClientSendCandidate(candidate)
+		}
+
+		// Now you have access to clientMsg.Type and clientMsg.Msg
+		//log.Printf("Received message of type '%s' %v %v", clientMsg.Type, clientMsg.sndidate, clientMsg.sdp)
+
+	}
+
+	s.activeConnectionsMutex.Lock()
+	delete(s.activeConnections, conn)
+	s.activeConnectionsMutex.Unlock()
+}
+
+func (s *Signaling) sendMessage(sender *websocket.Conn, msg []byte) {
+	s.wsMutex.Lock()
+	defer s.wsMutex.Unlock()
+	for conn := range s.activeConnections {
+		if conn == sender {
+			if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+				log.Println(err)
+				continue
+			}
+		}
+	}
+}
+
+func main() {
+
+	s := &Signaling{
+		activeConnections: make(map[*websocket.Conn]bool),
+		activeSubjects:    make(map[*websocket.Conn]string),
+
+		port: ":3111",
+		upgrader: websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	}
+
+	s.NatCon()
+
+	http.HandleFunc("/ws", s.handleWs)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./webclient/index.html")
+	})
+
+	http.HandleFunc("/view/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./webclient/consumer.html")
+	})
+
+	log.Printf("test web client running on port %s", s.port)
+	log.Fatal(http.ListenAndServe(s.port, nil))
+}
+
+func (s *Signaling) ForwardAnswer(conn *websocket.Conn, msg *nats.Msg) {
+	//forward to client
+	s.sendMessage(conn, msg.Data)
+}
+
+func (s *Signaling) ForwardCandidate(conn *websocket.Conn, msg *nats.Msg) {
+	//forward to client
+
+	s.sendMessage(conn, msg.Data)
+}
+
+func (s *Signaling) NatCon() {
+	url := os.Getenv("NATS_URL")
+	if url == "" {
+		url = "nats://localhost:4222"
+	}
+
+	nc, err := nats.Connect(url)
+	if err != nil {
+		log.Printf("Error: unbale to connect to NATS server %v", err)
+	}
+
+	log.Printf("Connected to NATS server at %s", url)
+
+	// Close the NATS connection when done
+	//defer nc.Close()
+
+	s.nat = nc
+}
